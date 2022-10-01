dd6aaa058f1317c8b3eeb80f7f93b6ca255268f7
Rishabh Singh <rishabhcena1@gmail.com>
2022-10-02 02:04:08 +0530

    major revamp

--- src/js/main.js
+++ src/js/main.js
@@ -317,45 +317,45 @@
 
    /* smoothscroll
     * ------------------------------------------------------ */
-    const ssMoveTo = function(){
+    // const ssMoveTo = function(){
 
-        const easeFunctions = {
-            easeInQuad: function (t, b, c, d) {
-                t /= d;
-                return c * t * t + b;
-            },
-            easeOutQuad: function (t, b, c, d) {
-                t /= d;
-                return -c * t* (t - 2) + b;
-            },
-            easeInOutQuad: function (t, b, c, d) {
-                t /= d/2;
-                if (t < 1) return c/2*t*t + b;
-                t--;
-                return -c/2 * (t*(t-2) - 1) + b;
-            },
-            easeInOutCubic: function (t, b, c, d) {
-                t /= d/2;
-                if (t < 1) return c/2*t*t*t + b;
-                t -= 2;
-                return c/2*(t*t*t + 2) + b;
-            }
-        }
+    //     const easeFunctions = {
+    //         easeInQuad: function (t, b, c, d) {
+    //             t /= d;
+    //             return c * t * t + b;
+    //         },
+    //         easeOutQuad: function (t, b, c, d) {
+    //             t /= d;
+    //             return -c * t* (t - 2) + b;
+    //         },
+    //         easeInOutQuad: function (t, b, c, d) {
+    //             t /= d/2;
+    //             if (t < 1) return c/2*t*t + b;
+    //             t--;
+    //             return -c/2 * (t*(t-2) - 1) + b;
+    //         },
+    //         easeInOutCubic: function (t, b, c, d) {
+    //             t /= d/2;
+    //             if (t < 1) return c/2*t*t*t + b;
+    //             t -= 2;
+    //             return c/2*(t*t*t + 2) + b;
+    //         }
+    //     }
 
-        const triggers = document.querySelectorAll('.smoothscroll');
+    //     const triggers = document.querySelectorAll('.smoothscroll');
         
-        const moveTo = new MoveTo({
-            tolerance: 0,
-            duration: 1200,
-            easing: 'easeInOutCubic',
-            container: window
-        }, easeFunctions);
+    //     const moveTo = new MoveTo({
+    //         tolerance: 0,
+    //         duration: 1200,
+    //         easing: 'easeInOutCubic',
+    //         container: window
+    //     }, easeFunctions);
 
-        triggers.forEach(function(trigger) {
-            moveTo.registerTrigger(trigger);
-        });
+    //     triggers.forEach(function(trigger) {
+    //         moveTo.registerTrigger(trigger);
+    //     });
 
-    }; // end ssMoveTo
+    // }; // end ssMoveTo
 
 
 
@@ -371,7 +371,7 @@
         ssSwiper();
         ssAlertBoxes();
         ssBackToTop();
-        ssMoveTo();
+        // ssMoveTo();
 
     })();
 
