86c9338b1a5dd6888d040f3ed6e18edc8118615f
Rishabh Singh <rishabhcena1@gmail.com>
2022-10-02 13:44:35 +0530

    removed unused libs

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 /* ===================================================================
- * Augustine 1.0.0 - Main JS
+ * 1.0.0 - Main JS
  *
  *
  * ------------------------------------------------------------------- */
@@ -135,70 +135,6 @@
 
     }; // end ssStickyHeader
 
-
-
-   /* photoswipe
-    * ----------------------------------------------------- */
-    const ssPhotoswipe = function() {
-
-        const items = [];
-        const pswp = document.querySelectorAll('.pswp')[0];
-        const folioItems = document.querySelectorAll('.folio-item');
-
-        if (!(pswp && folioItems)) return;
-
-        folioItems.forEach(function(folioItem) {
-
-            let folio = folioItem;
-            let thumbLink = folio.querySelector('.folio-item__thumb-link');
-            let title = folio.querySelector('.folio-item__title');
-            let caption = folio.querySelector('.folio-item__caption');
-            let titleText = '<h4>' + title.innerHTML + '</h4>';
-            let captionText = caption.innerHTML;
-            let href = thumbLink.getAttribute('href');
-            let size = thumbLink.dataset.size.split('x'); 
-            let width  = size[0];
-            let height = size[1];
-
-            let item = {
-                src  : href,
-                w    : width,
-                h    : height
-            }
-
-            if (caption) {
-                item.title = titleText.trim() + captionText.trim();
-            }
-
-            items.push(item);
-
-        });
-
-        // bind click event
-        folioItems.forEach(function(folioItem, i) {
-
-            let thumbLink = folioItem.querySelector('.folio-item__thumb-link');
-
-            thumbLink.addEventListener('click', function(e) {
-
-                e.preventDefault();
-
-                let options = {
-                    index: i,
-                    showHideOpacity: true
-                }
-
-                // initialize PhotoSwipe
-                let lightBox = new PhotoSwipe(pswp, PhotoSwipeUI_Default, items, options);
-                lightBox.init();
-            });
-
-        });
-
-    };  // end ssPhotoSwipe
-
-
-
    /* animate elements if in viewport
     * ------------------------------------------------------ */
     const ssAnimateOnScroll = function() {
@@ -239,58 +175,6 @@
 
     }; // end ssAnimateOnScroll
 
-
-
-   /* swiper
-    * ------------------------------------------------------ */ 
-    const ssSwiper = function() {
-
-        const mySwiper = new Swiper('.swiper', {
-
-            slidesPerView: 1,
-            effect: 'slide',
-            spaceBetween: 160,
-            centeredSlides: true,
-            speed: 1000,
-            navigation: {
-                nextEl: ".testimonial-slider__next",
-                prevEl: ".testimonial-slider__prev",
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            }
-
-        });
-
-    }; // end ssSwiper
-
-
-
-   /* alert boxes
-    * ------------------------------------------------------ */
-    const ssAlertBoxes = function() {
-
-        const boxes = document.querySelectorAll('.alert-box');
-  
-        boxes.forEach(function(box){
-
-            box.addEventListener('click', function(e) {
-                if (e.target.matches('.alert-box__close')) {
-                    e.stopPropagation();
-                    e.target.parentElement.classList.add('hideit');
-
-                    setTimeout(function(){
-                        box.style.display = 'none';
-                    }, 500)
-                }
-            });
-        })
-
-    }; // end ssAlertBoxes
-
-
-
     /* back to top
     * ------------------------------------------------------ */
     const ssBackToTop = function() {
@@ -313,65 +197,14 @@
 
     }; // end ssBackToTop
 
-
-
-   /* smoothscroll
-    * ------------------------------------------------------ */
-    // const ssMoveTo = function(){
-
-    //     const easeFunctions = {
-    //         easeInQuad: function (t, b, c, d) {
-    //             t /= d;
-    //             return c * t * t + b;
-    //         },
-    //         easeOutQuad: function (t, b, c, d) {
-    //             t /= d;
-    //             return -c * t* (t - 2) + b;
-    //         },
-    //         easeInOutQuad: function (t, b, c, d) {
-    //             t /= d/2;
-    //             if (t < 1) return c/2*t*t + b;
-    //             t--;
-    //             return -c/2 * (t*(t-2) - 1) + b;
-    //         },
-    //         easeInOutCubic: function (t, b, c, d) {
-    //             t /= d/2;
-    //             if (t < 1) return c/2*t*t*t + b;
-    //             t -= 2;
-    //             return c/2*(t*t*t + 2) + b;
-    //         }
-    //     }
-
-    //     const triggers = document.querySelectorAll('.smoothscroll');
-        
-    //     const moveTo = new MoveTo({
-    //         tolerance: 0,
-    //         duration: 1200,
-    //         easing: 'easeInOutCubic',
-    //         container: window
-    //     }, easeFunctions);
-
-    //     triggers.forEach(function(trigger) {
-    //         moveTo.registerTrigger(trigger);
-    //     });
-
-    // }; // end ssMoveTo
-
-
-
    /* initialize
     * ------------------------------------------------------ */
     (function ssInit() {
-
         ssPreloader();
         ssMobileMenu();
         ssStickyHeader();
-        ssPhotoswipe();
         ssAnimateOnScroll();
-        ssSwiper();
-        ssAlertBoxes();
         ssBackToTop();
-        // ssMoveTo();
 
     })();
 
